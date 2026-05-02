db8bdc4fbb5562fd1bb002e2a9b418465f703e9f
zyp <zyp928@foxmail.com>
2026-05-02 16:34:33 +0800

    防止过度发布

---
+++ TodoApi/TodoItemDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApi;
+
+public class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(Todo todo)
+    {
+        Id = todo.Id;
+        Name = todo.Name;
+        IsComplete = todo.IsComplete;
+    }
+}
